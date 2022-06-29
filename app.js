25d2b81ba662ce31eb171dc2a625225a5d73fa6b
Ugur Y <uguryuruk@yahoo.com>
2022-06-29 20:38:39 +0300

    clean blog ikinci odev done

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//static dosyaların klasörünü belirtir. MIDDLEWARES
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-  res.send(blog);
+  //statik html sayfası göndermek için, klasörden dosyayı buluyoruz!
+  //   res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
 });
 
 const port = 3000;
